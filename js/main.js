c586ffbed7226e12c55d83e55ae2339bc3b3c352
jery lenas <jerylenas@gmail.com>
2022-09-21 19:20:58 +0700

    fix navigation click on current device size

--- js/main.js
+++ js/main.js
@@ -24,26 +24,26 @@ const navItem = document.querySelectorAll(".nav_item");
 // arrowUp.src = "../images/icon-arrow-dark.svg";
 // arrowUp.style = "width:10px; height:10px; transform:rotate(180deg)";
 
+var width = Math.max(window.screen.width, window.innerWidth);
 window.addEventListener("resize", (event) => {
-	var width = Math.max(window.screen.width, window.innerWidth);
-	console.log(width);
+	width = Math.max(window.screen.width, window.innerWidth);
+});
+
+const navItemDropdown = document.getElementsByClassName("nav_item_dropdown");
+navItem.forEach((element) => {
+	let button = element.getElementsByClassName("nav_item_content")[0];
+	let child = button.getElementsByClassName("nav_item_arrow")[0];
 	if (width <= 767) {
-		const navItemDropdown =
-			document.getElementsByClassName("nav_item_dropdown");
-		navItem.forEach((element) => {
-			let button = element.getElementsByClassName("nav_item_content")[0];
-			let child = button.getElementsByClassName("nav_item_arrow")[0];
-			button.addEventListener("click", function () {
-				let itemClass = this.nextElementSibling.className;
-				for (i = 0; i < navItemDropdown.length; i++) {
-					navItemDropdown[i].className = navDropdownClose;
-					child.className = "nav_item_arrow";
-				}
-				if (itemClass === navDropdownClose) {
-					this.nextElementSibling.className = navDropdownOpen;
-					child.className = "nav_item_arrow arrow_up";
-				}
-			});
+		button.addEventListener("click", function () {
+			let itemClass = this.nextElementSibling.className;
+			for (i = 0; i < navItemDropdown.length; i++) {
+				navItemDropdown[i].className = navDropdownClose;
+				child.className = "nav_item_arrow";
+			}
+			if (itemClass === navDropdownClose) {
+				this.nextElementSibling.className = navDropdownOpen;
+				child.className = "nav_item_arrow arrow_up";
+			}
 		});
 	}
 });
